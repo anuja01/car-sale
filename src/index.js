f24be7f7a3d122e1f9f3673f716a5d3dfd0032f8
Anuja Ulpathakumbura <anuja.ulp@gmail.com>
2020-11-22 22:00:36 +0800

    setup redux middleware and directory strucure for redux

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import App from './App';
+/*
+    keep track of the globle store which keeps the state
+    provider enables the store to anywhere of the app
+*/
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import reducers from "./reducers";
+
+import App from "./App";
+
+const store = createStore(reducers, compose(applyMiddleware(thunk))); // setup thunk middleware with redux
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
