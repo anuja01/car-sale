dc0538068768ef54fa3d86c63ba96f4ed57f4839
Anuja Ulpathakumbura <anuja.ulp@gmail.com>
2021-07-04 23:51:17 +0800

    initial setup with Miragejs for mocks

--- src/index.js
+++ src/index.js
@@ -1,23 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-/*
-    keep track of the globle store which keeps the state
-    provider enables the store to anywhere of the app
-*/
-import { Provider } from "react-redux";
-import { createStore, applyMiddleware, compose } from "redux";
-import thunk from "redux-thunk";
-
-import reducers from "./reducers";
-
-import App from "./App";
-
-const store = createStore(reducers, compose(applyMiddleware(thunk))); // setup thunk middleware with redux
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <Provider store={store}>
+  <React.StrictMode>
     <App />
-  </Provider>,
-  document.getElementById("root")
+  </React.StrictMode>,
+  document.getElementById('root')
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-
-import posts from './posts';
-
-export default combineReducers({
-    // combine all the reducers here
-    posts,
-})
